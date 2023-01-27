a78fe2e38c2183f33348f282267bb4fd6997e304
Shriya <shriya0690@gmail.com>
2023-01-27 16:27:00 +0530

    Initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {  BrowserRouter, Routes, Route} from 'react-router-dom';
+import Login from "./auth/Login"
+import Signup from './auth/Signup';
+  
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path='/' element={< Login />}></Route> 
+          <Route exact path='/signup' element={< Signup />}></Route> 
+        </Routes>
+     </BrowserRouter>
     </div>
   );
 }
